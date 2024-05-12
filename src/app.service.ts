ee5cdaf51404d1a27711e533b61454dfbb810b13
Uszakow <pawel.uszakow@accenture.com>
2024-05-12 12:25:46 +0200

    mime lib implementation

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common'
+import mime from 'mime'
 
 @Injectable()
 export class AppService {
 	getHello(): string {
+		const mimeType = mime.getType('txt')
+		console.log('mimeType', mimeType)
+
 		return 'Hello World!'
 	}
 }
